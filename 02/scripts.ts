7b7c79fd4c4c63f00b8cad555600e733c050ffdd
Arnaldo <costanetoow@gmail.com>
2023-08-22 22:52:06 -0300

    feat: exercicios finalizados

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -1,21 +1,68 @@
 type Item = {
-    nome: string,
-    descricao: string,
-    valor: number
-}
+	nome: string;
+	descricao: string;
+	valor: number;
+};
 
 type Cartao = {
-    numero: number,
-    validade: string,
-    nome: string,
-    cvv: number
-}
+	numero: number;
+	validade: string;
+	nome: string;
+	cvv: number;
+};
 
 type Carrinho = {
-    item: Item,
-    qtd: number,
-    desconto: number,
-    frete: number,
-    tipoTransacao: 'credito' | 'Debito',
-    cartao: Cartao
-}
+	item: Item;
+	qtd: number;
+	desconto: number;
+	frete: number;
+	tipoTransacao: 'credito' | 'Debito';
+	cartao: Cartao;
+};
+
+type Endereco = {
+	cep: string;
+	rua: string;
+	bairro: string;
+	cidade: string;
+	estado: string;
+};
+
+type NovoCarrinho = Omit<Carrinho, 'tipoTransacao'> & {
+	endereco: Endereco;
+	tipoTransacao: Lowercase<'credito' | 'Debito'>;
+};
+
+// type NovoCarrinho = {
+// 	item: Item;
+// 	qtd: number;
+// 	desconto: number;
+// 	frete: number;
+// 	tipoTransacao: Lowercase<'credito' | 'Debito'>;
+// 	cartao: Cartao;
+// };
+
+const venda: NovoCarrinho = {
+	item: {
+		nome: 'Nome',
+		descricao: 'Descricao',
+		valor: 123,
+	},
+	qtd: 3,
+	desconto: 10,
+	frete: 1000,
+	tipoTransacao: 'credito',
+	cartao: {
+		numero: 123,
+		validade: 'string',
+		cvv: 779,
+		nome: 'nome',
+	},
+	endereco: {
+		cep: '47075220',
+		bairro: 'brotas',
+		cidade: 'Salvador',
+		estado: 'BA',
+		rua: 'Centro',
+	},
+};
--- 03/scripts.ts
+++ 03/scripts.ts
@@ -1,15 +1,30 @@
 type Conn = {
-    username: string,
-    password: string,
-    host: string,
-    port: string,
-    dbname: string
-}
+	username: string;
+	password: string;
+	host: string;
+	port: string;
+	dbname: string;
+};
 
-const conexao = (dados: Conn) => {
-    const { username, password, host, port, dbname } = dados;
-    return {
-        driver: 'postgres',
-        url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
-    };
-}
+type Conexao = {
+	driver: string;
+	url: string;
+};
+
+const conexao = (dados: Conn): Readonly<Conexao> => {
+	const { username, password, host, port, dbname } = dados;
+	return {
+		driver: 'postgres',
+		url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`,
+	};
+};
+
+const conecta = conexao({
+	username: 'string',
+	password: 'string',
+	host: 'string',
+	port: 'string',
+	dbname: 'string',
+});
+
+conecta.driver = 'mysql';
